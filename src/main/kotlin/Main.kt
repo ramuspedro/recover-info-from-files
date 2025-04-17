7dcdbd315b9bd25adad7cb67a8e2aa9c938d7704
Pedro Ramos <ramuspedro@gmail.com>
2025-04-16 22:01:41 -0300

    add: read from folder and write on a specific file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,33 @@
 package org.project
 
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.stream.Collectors
+import kotlin.io.path.name
+import kotlin.io.path.pathString
+
 fun main() {
-    println("Hello World!")
+    val typeOfFile = "kt"
+    val mainPath = "C:\\Users\\hilana.pereira\\Pedro\\Projetos\\codecrafters\\codecrafters-redis-kotlin"
+    val resultFileName = "result.md" // will be generated on the root f this project
+    File(resultFileName).bufferedWriter().use { outResult ->
+        Files
+            .walk(Paths.get(mainPath))
+            .collect(Collectors.toList())
+            .forEach { filePath ->
+                if (filePath.name.takeLast(typeOfFile.length) == typeOfFile) {
+                    outResult.write(filePath.pathString)
+                    outResult.newLine()
+                    outResult.write("```$typeOfFile")
+                    outResult.newLine()
+                    File(filePath.pathString).forEachLine { outKt ->
+                        outResult.write(outKt)
+                        outResult.newLine()
+                    }
+                    outResult.write("```")
+                    outResult.newLine()
+                }
+            }
+    }
 }
